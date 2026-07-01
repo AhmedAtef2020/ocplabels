4009a98a203f70502b854ddbb84a0bb4406fdc35
Ahmed Atef <117845266+AhmedAtef2020@users.noreply.github.com>
2026-07-01 17:02:18 +0300

    Loaded for vercel

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useRef, useState } from 'react'
+import Confetti from 'js-confetti'
+import './styles/style.css'
+import LabelsPrinter from "./LabelsPrinter"
+
+const confetti = new Confetti()
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    confetti.addConfetti()
+    setCount(c => c + 1)
+  }
+
+  return (
+    <LabelsPrinter />
+    // <button className="btn" onClick={handleClick}>
+    //   <span role="img" aria-label="react-emoji">⚛️</span> {count}
+    // </button>
+  )
+}
+
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import App from './App.jsx'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App)
+  )
+)
